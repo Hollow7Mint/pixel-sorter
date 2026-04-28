c4c4f585dc10bedb58510b7bd0af021b83be7540
James Holloway <james.holloway94@gmail.com>
2026-04-28 21:40:02 +0000

    unify db sync record with metadata heartbeat

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-28 20:31:54 UTC
+# Last sync: 2026-04-28 21:40:02 UTC
