6ea73b540f3cb3c2d3695127a7d5447313f4f6f4
James Holloway <james.holloway94@gmail.com>
2026-04-28 23:48:06 +0000

    preserve ssh certificate revocation sync in metadata

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-28 22:53:27 UTC
+# Last sync: 2026-04-28 23:48:06 UTC
