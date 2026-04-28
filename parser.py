fd8d3cb7144826ad28365e8e5642d0cbcb4cc51a
James Holloway <james.holloway94@gmail.com>
2026-04-28 08:57:20 +0000

    sync ssm parameter refresh after pipeline pass

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-28 07:41:01 UTC
+# Last sync: 2026-04-28 08:57:20 UTC
