4644adfc0f5bf7f25a602e48a791e862a96eaadc
James Holloway <james.holloway94@gmail.com>
2026-04-29 18:04:45 +0000

    finalize mongodb collection sync after nightly apply

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-29 16:50:24 UTC
+# Last sync: 2026-04-29 18:04:45 UTC
