0e6aa6ffcb15c58c5d4fd342aec5f7c5234e4042
James Holloway <james.holloway94@gmail.com>
2026-04-29 11:40:12 +0000

    reload postgres autovacuum sync marker for audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-29 10:50:17 UTC
+# Last sync: 2026-04-29 11:40:12 UTC
