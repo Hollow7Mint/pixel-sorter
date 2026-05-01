b2fb0e4e6a87e8ebe02e08d4be10e8ae7c042ff3
James Holloway <james.holloway94@gmail.com>
2026-05-01 16:00:55 +0000

    finish secrets manager rotation heartbeat after sync

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-01 14:35:11 UTC
+# Last sync: 2026-05-01 16:00:55 UTC
