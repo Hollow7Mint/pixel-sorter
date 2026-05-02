98505cca402d5d9bdc7b304709ec7ae82185b038
James Holloway <james.holloway94@gmail.com>
2026-05-02 09:55:16 +0000

    persist tunnel config refresh in ssh banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-02 08:56:01 UTC
+# Last sync: 2026-05-02 09:55:16 UTC
