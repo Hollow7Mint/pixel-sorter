4fc602501dd58f27c52b7476a480606ca65a482a
James Holloway <james.holloway94@gmail.com>
2026-05-03 16:56:36 +0000

    trace mysql table statistics sync for fleet audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-03 15:30:22 UTC
+# Last sync: 2026-05-03 16:56:35 UTC
