e980ee803778b5e795420506bbd5cace82d238be
James Holloway <james.holloway94@gmail.com>
2026-05-04 01:19:43 +0000

    advance mysql gtid sync into config record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-04 00:09:12 UTC
+# Last sync: 2026-05-04 01:19:43 UTC
