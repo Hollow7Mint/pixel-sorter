0a037f2a89f13f8ab3a6117fd0ee94c4add203c7
James Holloway <james.holloway94@gmail.com>
2026-05-06 09:20:20 +0000

    inspect iam trust policy sync marker for review

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-06 08:43:02 UTC
+# Last sync: 2026-05-06 09:20:20 UTC
