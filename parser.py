73f48f080cd80b1f697edc3ab0a710712c632108
James Holloway <james.holloway94@gmail.com>
2026-05-06 05:15:44 +0000

    preserve nat gateway sync time in infra banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-06 04:46:48 UTC
+# Last sync: 2026-05-06 05:15:44 UTC
