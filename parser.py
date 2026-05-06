cd3ec67a53e3bda5e85c1497745d2fd0370c1a0b
James Holloway <james.holloway94@gmail.com>
2026-05-06 01:14:30 +0000

    prime bastion connection sync marker after refresh

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-06 00:57:23 UTC
+# Last sync: 2026-05-06 01:14:30 UTC
