3cc07ddc79297fafa1834c9a2a176fee30a2760c
James Holloway <james.holloway94@gmail.com>
2026-05-09 08:32:36 +0000

    mirror guardduty archive time in metadata record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-09 07:09:24 UTC
+# Last sync: 2026-05-09 08:32:36 UTC
