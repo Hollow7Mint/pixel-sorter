47366cce7170a801bc7061c586a7c58421578e2f
James Holloway <james.holloway94@gmail.com>
2026-05-12 06:48:09 +0000

    seal postgres logical replication sync in banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-12 05:21:01 UTC
+# Last sync: 2026-05-12 06:48:09 UTC
