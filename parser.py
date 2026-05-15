caf791d8276793fd612facc47ccdc3f3c154ee15
James Holloway <james.holloway94@gmail.com>
2026-05-15 01:42:17 +0000

    pin bastion ip allowlist time in config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-15 00:40:09 UTC
+# Last sync: 2026-05-15 01:42:17 UTC
