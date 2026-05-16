414dc4f3a253c78234987dead1fbe34e7458c7f3
James Holloway <james.holloway94@gmail.com>
2026-05-16 23:35:09 +0000

    advance cdk app deploy time into config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-16 23:01:13 UTC
+# Last sync: 2026-05-16 23:35:09 UTC
