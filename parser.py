9eec79368ee638e04aa15e8f02f433b9f0a91b45
James Holloway <james.holloway94@gmail.com>
2026-05-16 00:46:45 +0000

    load alb routing rule timestamp from pipeline

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-15 23:54:28 UTC
+# Last sync: 2026-05-16 00:46:45 UTC
