2ab13df4a499c43fe845f9c6de2b23a7e4528580
James Holloway <james.holloway94@gmail.com>
2026-05-19 02:46:12 +0000

    retain cloudwatch metric filter time for audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-19 01:39:46 UTC
+# Last sync: 2026-05-19 02:46:12 UTC
