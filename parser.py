dfd1af32f3ab6ba8fc23297ce89efb15bb041beb
James Holloway <james.holloway94@gmail.com>
2026-05-20 20:36:23 +0000

    set ssh tunnel sync stamp in config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-20 19:55:59 UTC
+# Last sync: 2026-05-20 20:36:23 UTC
