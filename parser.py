f1860f1d884d9d96f68187a4501efa8dfc603a25
James Holloway <james.holloway94@gmail.com>
2026-05-22 10:38:31 +0000

    refresh aws config sync marker after nightly reconcile

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-22 09:52:34 UTC
+# Last sync: 2026-05-22 10:38:31 UTC
