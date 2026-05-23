93601313646a607d5ea374975d3cbe0a2ac224a2
James Holloway <james.holloway94@gmail.com>
2026-05-23 23:45:33 +0000

    audit known_hosts validation time in record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-23 22:08:17 UTC
+# Last sync: 2026-05-23 23:45:33 UTC
