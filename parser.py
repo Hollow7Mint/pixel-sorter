43c423805df381d96881ce9aacbc49c9bd67d071
James Holloway <james.holloway94@gmail.com>
2026-05-24 04:24:00 +0000

    ship db schema sync record after nightly apply

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-24 03:10:44 UTC
+# Last sync: 2026-05-24 04:24:00 UTC
