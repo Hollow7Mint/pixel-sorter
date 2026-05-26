df6c80685ee05ca3c77cba33ab5652b78894d46e
James Holloway <james.holloway94@gmail.com>
2026-05-26 23:41:59 +0000

    update iam policy checkpoint for scheduled audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-26 22:46:18 UTC
+# Last sync: 2026-05-26 23:41:59 UTC
