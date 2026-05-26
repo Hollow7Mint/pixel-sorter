c1c034ac5291739ceec98aeea7f457026c4a1141
James Holloway <james.holloway94@gmail.com>
2026-05-26 05:08:56 +0000

    ship parameter store refresh record after nightly run

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-26 04:52:07 UTC
+# Last sync: 2026-05-26 05:08:56 UTC
