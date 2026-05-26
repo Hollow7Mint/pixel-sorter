b414946e55bd2ee6be174d9b1280e5d69172edeb
James Holloway <james.holloway94@gmail.com>
2026-05-26 01:03:49 +0000

    frame ssh agent socket sync in config record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-25 23:42:07 UTC
+# Last sync: 2026-05-26 01:03:49 UTC
