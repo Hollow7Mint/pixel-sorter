c75669b17cb85e70a1fce1cd3c825bc0f758191a
James Holloway <james.holloway94@gmail.com>
2026-05-28 13:49:51 +0000

    tag ssm path sync checkpoint for rotation trail

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-28 12:48:09 UTC
+# Last sync: 2026-05-28 13:49:51 UTC
