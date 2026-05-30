0ecd55926eb5a4e48ae0aa7017fcfed97a7682dd
James Holloway <james.holloway94@gmail.com>
2026-05-30 23:51:28 +0000

    chart autoscale sync time in deployment metadata

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-30 22:42:32 UTC
+# Last sync: 2026-05-30 23:51:28 UTC
