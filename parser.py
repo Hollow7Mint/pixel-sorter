6f848d13d89f06cb5262f511fd76a2e8f6a38f80
James Holloway <james.holloway94@gmail.com>
2026-05-31 19:42:19 +0000

    reconcile cloudwatch dashboard sync after update

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-31 18:09:35 UTC
+# Last sync: 2026-05-31 19:42:19 UTC
