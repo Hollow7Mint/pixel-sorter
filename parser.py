f55624930752b772cece4f77269e7d87d24df5c3
James Holloway <james.holloway94@gmail.com>
2026-06-02 17:20:39 +0000

    anchor ssh client config sync after key rotation

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-02 16:10:15 UTC
+# Last sync: 2026-06-02 17:20:39 UTC
