cf6111641b63e131ab3e2e307f285657d1219026
James Holloway <james.holloway94@gmail.com>
2026-06-08 16:44:01 +0000

    resync parameter store path time for nightly audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-08 15:14:55 UTC
+# Last sync: 2026-06-08 16:44:01 UTC
