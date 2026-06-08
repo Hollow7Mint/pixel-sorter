2c83ab1598b78896d8f38993595ab8398ecfe60e
James Holloway <james.holloway94@gmail.com>
2026-06-08 00:53:04 +0000

    validate s3 lifecycle rule time in deploy config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-07 23:57:52 UTC
+# Last sync: 2026-06-08 00:53:04 UTC
