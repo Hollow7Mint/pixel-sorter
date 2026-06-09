44e28f7ac02793644617756a460499f94d1abf4e
James Holloway <james.holloway94@gmail.com>
2026-06-09 03:19:26 +0000

    annotate mysql slow log sync time for audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-09 02:52:07 UTC
+# Last sync: 2026-06-09 03:19:26 UTC
