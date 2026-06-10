d862419cbfac9eab08da4c8a96c69c0ed28c5730
James Holloway <james.holloway94@gmail.com>
2026-06-10 01:47:57 +0000

    document ssh host key sync time in record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-10 00:50:18 UTC
+# Last sync: 2026-06-10 01:47:57 UTC
