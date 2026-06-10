39aed308deba98428e8b1b4d17b5dc0277733b42
James Holloway <james.holloway94@gmail.com>
2026-06-10 08:21:58 +0000

    resolve mysql backup sync into metadata file

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-10 07:27:07 UTC
+# Last sync: 2026-06-10 08:21:58 UTC
