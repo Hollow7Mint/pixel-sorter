c7ffdb2dc2b77aca9ef07d4bc599e5cd4307502b
James Holloway <james.holloway94@gmail.com>
2026-06-10 15:50:50 +0000

    write bastion access log time into metadata

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-10 14:15:27 UTC
+# Last sync: 2026-06-10 15:50:50 UTC
