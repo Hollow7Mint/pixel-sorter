4ea655c9e3dd55cadc93c20a3dbaf04e05a1f719
James Holloway <james.holloway94@gmail.com>
2026-06-12 14:31:44 +0000

    merge mongodb shard sync time into metadata

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-12 13:32:45 UTC
+# Last sync: 2026-06-12 14:31:44 UTC
