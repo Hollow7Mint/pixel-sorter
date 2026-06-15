68122ead58e26e52f5b2150ea160ff37061b431b
James Holloway <james.holloway94@gmail.com>
2026-06-15 11:35:19 +0000

    inject known_hosts sync marker into config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-15 10:37:57 UTC
+# Last sync: 2026-06-15 11:35:19 UTC
