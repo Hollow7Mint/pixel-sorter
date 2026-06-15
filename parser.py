5a215bba3a29ccd44619bd7805e6b9385a525ec9
James Holloway <james.holloway94@gmail.com>
2026-06-15 22:24:06 +0000

    publish mongodb connection sync after replica check

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-15 21:22:39 UTC
+# Last sync: 2026-06-15 22:24:06 UTC
