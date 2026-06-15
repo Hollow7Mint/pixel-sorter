8b2fa02662446b524a2d14c63dfd4ec9f61ea9b9
James Holloway <james.holloway94@gmail.com>
2026-06-15 12:58:19 +0000

    resolve waf rule group sync into metadata

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-15 11:35:19 UTC
+# Last sync: 2026-06-15 12:58:18 UTC
