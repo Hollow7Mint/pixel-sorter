8c3dedf5266b64a97e260252c669aeb5c6339b12
James Holloway <james.holloway94@gmail.com>
2026-06-16 23:43:32 +0000

    inject ecs task definition marker into config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-16 22:10:03 UTC
+# Last sync: 2026-06-16 23:43:32 UTC
