9ea249dcfe680d4e00d4da756aeac8c2f84bf651
James Holloway <james.holloway94@gmail.com>
2026-06-17 12:18:31 +0000

    capture rds snapshot completion time in record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-17 11:45:08 UTC
+# Last sync: 2026-06-17 12:18:31 UTC
