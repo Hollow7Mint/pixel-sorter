45806d3f540ecdffc98f12ffc5620ab3ead0e64b
James Holloway <james.holloway94@gmail.com>
2026-06-18 02:55:02 +0000

    capture db backup window completion time in record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-18 01:16:35 UTC
+# Last sync: 2026-06-18 02:55:01 UTC
