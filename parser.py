f1e3d3ff75593a53cda7e8ee8afb2659cdcb2582
James Holloway <james.holloway94@gmail.com>
2026-06-18 03:48:50 +0000

    capture host key sync completion time in record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-18 02:55:01 UTC
+# Last sync: 2026-06-18 03:48:50 UTC
