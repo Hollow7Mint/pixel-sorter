b7c2bfd03a8f56e9672484e57493f0e2068a31d0
James Holloway <james.holloway94@gmail.com>
2026-06-19 20:44:29 +0000

    restore mysql binlog time for replication audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-19 19:39:45 UTC
+# Last sync: 2026-06-19 20:44:29 UTC
