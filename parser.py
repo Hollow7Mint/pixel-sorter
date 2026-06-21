53ef7f8fff9e58e24fab25f23dcdbc3f9f80fce0
James Holloway <james.holloway94@gmail.com>
2026-06-21 21:00:39 +0000

    load mysql slow query log sync from pipeline

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-21 19:13:14 UTC
+# Last sync: 2026-06-21 21:00:39 UTC
