7bcf31eacee3a4d8c34cb4901f7fc20a840073e0
James Holloway <james.holloway94@gmail.com>
2026-06-22 06:34:44 +0000

    queue cloudfront distribution stamp after invalidation

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-22 05:21:16 UTC
+# Last sync: 2026-06-22 06:34:44 UTC
