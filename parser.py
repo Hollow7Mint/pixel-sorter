dd6857dbcc7801754b3e041f250d8b18cb8494a9
James Holloway <james.holloway94@gmail.com>
2026-06-23 02:28:00 +0000

    advance ssh key trust sync into config record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-23 01:57:44 UTC
+# Last sync: 2026-06-23 02:27:59 UTC
