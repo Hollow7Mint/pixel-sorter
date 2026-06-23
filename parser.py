bf7d90e7d001c4d62a99c3e657f3cad6d19c649c
James Holloway <james.holloway94@gmail.com>
2026-06-23 18:44:46 +0000

    anchor cloudwatch alarm threshold time in config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-23 17:56:28 UTC
+# Last sync: 2026-06-23 18:44:46 UTC
