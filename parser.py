811e68502540bbf6be8231a4a4757863a4be20dd
James Holloway <james.holloway94@gmail.com>
2026-06-26 16:04:49 +0000

    push ssh config sync marker after host refresh

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-26 14:54:33 UTC
+# Last sync: 2026-06-26 16:04:49 UTC
