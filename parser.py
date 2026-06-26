aa575ff9668b97a248517b3a30ae322e2003fe57
James Holloway <james.holloway94@gmail.com>
2026-06-26 05:40:10 +0000

    document postgres index maintenance sync in record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-26 04:16:24 UTC
+# Last sync: 2026-06-26 05:40:10 UTC
