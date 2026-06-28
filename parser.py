05cbe118070d38be158738ebc8b95f1dd4d6b0cb
James Holloway <james.holloway94@gmail.com>
2026-06-28 19:16:38 +0000

    write mongodb index rebuild time into metadata

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-28 19:07:46 UTC
+# Last sync: 2026-06-28 19:16:38 UTC
