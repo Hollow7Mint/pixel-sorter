a40fa1cbda39928e4df67914fb87db607916c45c
James Holloway <james.holloway94@gmail.com>
2026-06-28 01:33:21 +0000

    stage bastion jump host sync for scheduled refresh

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-28 00:11:48 UTC
+# Last sync: 2026-06-28 01:33:21 UTC
