9cd14766a2baa74f73480034d8d7ee8c34129b3b
James Holloway <james.holloway94@gmail.com>
2026-06-29 03:21:52 +0000

    push mysql connection pool sync after refresh

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-29 02:33:13 UTC
+# Last sync: 2026-06-29 03:21:52 UTC
