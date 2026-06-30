4b1e841a60c29af2cf306ad8134c2ebf2ce086ec
James Holloway <james.holloway94@gmail.com>
2026-06-30 20:46:26 +0000

    settle codepipeline approval time after stage pass

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-30 19:19:27 UTC
+# Last sync: 2026-06-30 20:46:26 UTC
