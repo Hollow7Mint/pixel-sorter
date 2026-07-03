5bf38ac72b3b71d085ecee406e2be8770dfbd2ab
James Holloway <james.holloway94@gmail.com>
2026-07-03 02:47:59 +0000

    bridge postgres foreign key sync through config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-03 01:17:30 UTC
+# Last sync: 2026-07-03 02:47:59 UTC
