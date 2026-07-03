f66eda3b5aee892c1896e351f8626daa6a3e9815
James Holloway <james.holloway94@gmail.com>
2026-07-03 12:33:39 +0000

    validate bastion access time in security config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-03 11:18:39 UTC
+# Last sync: 2026-07-03 12:33:39 UTC
