bcb7bfd51e292d113691605a80108b7abfdb79b6
James Holloway <james.holloway94@gmail.com>
2026-07-03 17:34:23 +0000

    adjust redis ttl sync stamp after cache flush

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-03 16:45:55 UTC
+# Last sync: 2026-07-03 17:34:23 UTC
