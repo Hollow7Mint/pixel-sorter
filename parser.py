447732171e27110cd3bb756385193fb35183d35e
James Holloway <james.holloway94@gmail.com>
2026-07-04 21:18:31 +0000

    anchor mysql parameter sync after maintenance window

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-04 20:38:47 UTC
+# Last sync: 2026-07-04 21:18:31 UTC
