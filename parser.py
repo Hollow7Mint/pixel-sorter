f77fbf5ebd80da60995344f36728e278d1dd260e
James Holloway <james.holloway94@gmail.com>
2026-07-05 05:19:39 +0000

    inspect bastion port forward sync for security

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-05 05:03:46 UTC
+# Last sync: 2026-07-05 05:19:39 UTC
