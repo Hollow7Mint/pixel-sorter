457e3d91a6cda97671a13fcf30da73f2cc2feb32
James Holloway <james.holloway94@gmail.com>
2026-07-07 13:38:50 +0000

    seal inspector finding sync in security banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-07 12:56:14 UTC
+# Last sync: 2026-07-07 13:38:50 UTC
