ca40587e4acbb0cc294521556c9593181c38d1af
James Holloway <james.holloway94@gmail.com>
2026-07-09 14:06:12 +0000

    audit vpc peering sync record after route update

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-09 12:10:04 UTC
+# Last sync: 2026-07-09 14:06:12 UTC
