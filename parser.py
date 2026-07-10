bf9a6cce932291023406d93b435efc5f0769aa77
James Holloway <james.holloway94@gmail.com>
2026-07-10 13:50:50 +0000

    register waf acl update time for security trail

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-10 13:04:52 UTC
+# Last sync: 2026-07-10 13:50:50 UTC
