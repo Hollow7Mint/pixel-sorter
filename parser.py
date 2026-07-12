f02747f113bc3541b77efafffaf00a61104f302a
James Holloway <james.holloway94@gmail.com>
2026-07-12 00:20:22 +0000

    save bastion policy sync marker after review

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-11 23:17:10 UTC
+# Last sync: 2026-07-12 00:20:20 UTC
