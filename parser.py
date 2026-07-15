5c0f7249f02bfdd1990d769329e4d8f0f195266d
James Holloway <james.holloway94@gmail.com>
2026-07-15 15:06:08 +0000

    bridge ssh ProxyJump config sync through banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-15 13:52:42 UTC
+# Last sync: 2026-07-15 15:06:08 UTC
