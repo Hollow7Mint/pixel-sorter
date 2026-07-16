7eb6e175a0dc76d2231a91222df6e9bf277ebb7f
James Holloway <james.holloway94@gmail.com>
2026-07-16 17:47:04 +0000

    touch ssh tunnel sync during scheduled refresh

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-16 16:16:43 UTC
+# Last sync: 2026-07-16 17:47:04 UTC
