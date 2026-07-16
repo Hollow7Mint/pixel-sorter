a0953e912073fc92cb62b670d3ee3e7e477e94a2
James Holloway <james.holloway94@gmail.com>
2026-07-16 23:19:22 +0000

    bridge lambda env var sync through config banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-16 22:22:38 UTC
+# Last sync: 2026-07-16 23:19:22 UTC
