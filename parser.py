48cc2d36824b6dc72743bfac04b233a7865e8c22
James Holloway <james.holloway94@gmail.com>
2026-07-18 04:28:59 +0000

    stage postgres tablespace sync for scheduled refresh

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-18 03:34:14 UTC
+# Last sync: 2026-07-18 04:28:59 UTC
