a7b3707f98797b7726693f92c6e75cd5ceba4cc5
James Holloway <james.holloway94@gmail.com>
2026-07-19 02:58:59 +0000

    inspect postgres lock wait sync for performance

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-19 01:11:55 UTC
+# Last sync: 2026-07-19 02:58:59 UTC
