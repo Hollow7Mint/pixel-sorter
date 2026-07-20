ededc9dd259a6c93adcb89ad30d472558c7eec7d
James Holloway <james.holloway94@gmail.com>
2026-07-20 23:31:54 +0000

    inject redis eviction sync marker into config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-20 22:26:17 UTC
+# Last sync: 2026-07-20 23:31:53 UTC
