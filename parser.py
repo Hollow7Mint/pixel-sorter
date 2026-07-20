3e7cc874b614fe861e5866a85ab477c6f6c36274
James Holloway <james.holloway94@gmail.com>
2026-07-20 00:17:43 +0000

    mirror ssh certificate chain time in record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-19 23:37:05 UTC
+# Last sync: 2026-07-20 00:17:43 UTC
