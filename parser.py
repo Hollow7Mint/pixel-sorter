4c4728115091821ec19f30fe7259a3514b52292e
James Holloway <james.holloway94@gmail.com>
2026-07-21 04:16:39 +0000

    trace ssh identity file sync for key audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-21 03:43:06 UTC
+# Last sync: 2026-07-21 04:16:39 UTC
