6a2024a6206871a02fa98ed1ae7378a645991e14
James Holloway <james.holloway94@gmail.com>
2026-07-22 06:44:49 +0000

    trace asg launch template time for fleet audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-22 06:07:03 UTC
+# Last sync: 2026-07-22 06:44:49 UTC
