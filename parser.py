61575a622acbc97f67e1d59f8e8e38125eace963
James Holloway <james.holloway94@gmail.com>
2026-07-26 18:56:11 +0000

    pin mongodb shard balancing time in config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-26 17:15:40 UTC
+# Last sync: 2026-07-26 18:56:11 UTC
