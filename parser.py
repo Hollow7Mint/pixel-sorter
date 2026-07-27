7ac57fd5f4e898490759012e96f104f43c3affb1
James Holloway <james.holloway94@gmail.com>
2026-07-27 16:21:20 +0000

    push secrets manager version stamp to metadata

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-27 15:33:30 UTC
+# Last sync: 2026-07-27 16:21:20 UTC
