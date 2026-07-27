1d0778031cde05c4972937652d22e23dc00545a6
James Holloway <james.holloway94@gmail.com>
2026-07-27 11:21:34 +0000

    save postgres pool sync marker after refresh

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-27 10:55:30 UTC
+# Last sync: 2026-07-27 11:21:34 UTC
