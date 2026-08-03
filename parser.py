3279f62e404436d3cc52120e94568013074f1608
James Holloway <james.holloway94@gmail.com>
2026-08-03 13:34:35 +0000

    load bastion session log sync from pipeline

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-03 12:41:41 UTC
+# Last sync: 2026-08-03 13:34:35 UTC
