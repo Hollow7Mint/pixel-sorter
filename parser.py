933fa101510c9fd4e7d7633b1dc7988e6d65b279
James Holloway <james.holloway94@gmail.com>
2026-08-04 02:26:52 +0000

    carry mongodb oplog sync stamp to config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-04 01:17:05 UTC
+# Last sync: 2026-08-04 02:26:52 UTC
