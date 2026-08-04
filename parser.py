4539cd832b656e5bd7cec887bc419304ca58a292
James Holloway <james.holloway94@gmail.com>
2026-08-04 11:53:16 +0000

    touch redis sentinel sync during scheduled refresh

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-04 10:55:15 UTC
+# Last sync: 2026-08-04 11:53:16 UTC
