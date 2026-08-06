5102d4a8d90360ded977d3e014942298ff30a3c9
James Holloway <james.holloway94@gmail.com>
2026-08-06 20:55:13 +0000

    normalize mysql charset sync time in metadata

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-06 19:22:49 UTC
+# Last sync: 2026-08-06 20:55:13 UTC
