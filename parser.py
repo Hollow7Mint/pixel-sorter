11d2e7ad6588baf6aeb0824cfaa15f6988f66318
James Holloway <james.holloway94@gmail.com>
2026-08-06 14:34:16 +0000

    unify ec2 placement sync with metadata heartbeat

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-06 13:48:22 UTC
+# Last sync: 2026-08-06 14:34:16 UTC
