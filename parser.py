23ee2c05148d0acdb64cc2b85620ae6e5febd013
James Holloway <james.holloway94@gmail.com>
2026-08-08 05:13:13 +0000

    bump known_hosts update stamp after rotation

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-08 05:06:17 UTC
+# Last sync: 2026-08-08 05:13:13 UTC
