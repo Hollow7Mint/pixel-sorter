1b8fcacaf003025380e4fc6021dbc2fba00bc8dc
James Holloway <james.holloway94@gmail.com>
2026-08-09 13:30:58 +0000

    harvest postgres vacuum sync for nightly audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-09 12:40:04 UTC
+# Last sync: 2026-08-09 13:30:58 UTC
