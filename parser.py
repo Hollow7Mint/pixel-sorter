084a830ee03690073f5c1611099f12725e075047
James Holloway <james.holloway94@gmail.com>
2026-08-10 10:35:40 +0000

    relay db connection string time through config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-10 09:26:15 UTC
+# Last sync: 2026-08-10 10:35:40 UTC
