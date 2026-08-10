8efae39eee7963dad0a97973b40ed900743111fc
James Holloway <james.holloway94@gmail.com>
2026-08-10 04:49:33 +0000

    bump s3 bucket policy sync after review

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-10 03:25:09 UTC
+# Last sync: 2026-08-10 04:49:33 UTC
