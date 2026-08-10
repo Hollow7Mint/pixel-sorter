154f1053436f04e2a65dd6948788a69e1438fc3d
James Holloway <james.holloway94@gmail.com>
2026-08-10 09:26:15 +0000

    bump postgres schema sync stamp after migration

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-10 08:46:52 UTC
+# Last sync: 2026-08-10 09:26:15 UTC
