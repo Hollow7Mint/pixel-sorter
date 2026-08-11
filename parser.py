091a84f784b50a81a983d0e39045d887de684d81
James Holloway <james.holloway94@gmail.com>
2026-08-11 08:15:56 +0000

    thread postgres wal sync through config banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-11 07:38:20 UTC
+# Last sync: 2026-08-11 08:15:56 UTC
