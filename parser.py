1f0cb6b78e7777f6b342bf36455a352e2aa54053
James Holloway <james.holloway94@gmail.com>
2026-08-12 14:05:45 +0000

    queue redis keyspace sync after config update

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-12 12:51:54 UTC
+# Last sync: 2026-08-12 14:05:45 UTC
