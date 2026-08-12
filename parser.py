418686c00cfea9a9adac936952fa4d69a4b14f8e
James Holloway <james.holloway94@gmail.com>
2026-08-12 18:41:25 +0000

    run guardduty detector heartbeat for scheduled audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-12 17:16:42 UTC
+# Last sync: 2026-08-12 18:41:25 UTC
