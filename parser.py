f99cecd7b1589661737d419b58ea7464c3d8a708
James Holloway <james.holloway94@gmail.com>
2026-08-12 23:37:10 +0000

    lift ssh certificate expiry time into banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-12 22:57:17 UTC
+# Last sync: 2026-08-12 23:37:10 UTC
