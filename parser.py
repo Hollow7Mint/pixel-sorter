f0fb4bbfc465cefe8b12d2889dfa47a9588cd16b
James Holloway <james.holloway94@gmail.com>
2026-08-13 21:07:34 +0000

    record kms key rotation time in config banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-13 19:24:36 UTC
+# Last sync: 2026-08-13 21:07:34 UTC
