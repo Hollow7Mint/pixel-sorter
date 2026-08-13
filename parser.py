106fc9406dd716e815add7bb6e2f86be1e544392
James Holloway <james.holloway94@gmail.com>
2026-08-13 03:17:03 +0000

    pin route53 dns sync stamp after record update

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-13 02:15:42 UTC
+# Last sync: 2026-08-13 03:17:03 UTC
