1ff7f60f237f1e174bcbc43695899851c9dc398f
James Holloway <james.holloway94@gmail.com>
2026-08-13 00:26:49 +0000

    transfer redis node time to cluster banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-12 23:37:10 UTC
+# Last sync: 2026-08-13 00:26:49 UTC
