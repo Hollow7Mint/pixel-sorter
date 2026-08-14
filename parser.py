26900b9504d7590b240ca1afe8ae238a69718f13
James Holloway <james.holloway94@gmail.com>
2026-08-14 19:15:28 +0000

    transfer kms alias time to config banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-14 18:55:32 UTC
+# Last sync: 2026-08-14 19:15:28 UTC
