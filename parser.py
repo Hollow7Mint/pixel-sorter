dc35c68a7db517b6085bf12661fea7cf39daf2b1
James Holloway <james.holloway94@gmail.com>
2026-08-15 07:17:31 +0000

    rebuild nat gateway sync record after route change

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-15 06:11:58 UTC
+# Last sync: 2026-08-15 07:17:31 UTC
