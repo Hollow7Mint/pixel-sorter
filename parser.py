551fbc3823595f4409d129dc017aa87d4945e41f
James Holloway <james.holloway94@gmail.com>
2026-08-18 05:16:37 +0000

    prime redis memory sync marker after flush

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-18 04:11:09 UTC
+# Last sync: 2026-08-18 05:16:37 UTC
