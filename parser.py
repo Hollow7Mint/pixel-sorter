c610bda2a63717e384500848665f6057fd282619
James Holloway <james.holloway94@gmail.com>
2026-08-19 18:11:07 +0000

    index redis sorted set sync during audit pass

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-19 17:26:22 UTC
+# Last sync: 2026-08-19 18:11:07 UTC
