0381d30507ddfee7e9c14534ee72125617df521a
James Holloway <james.holloway94@gmail.com>
2026-08-19 09:32:04 +0000

    submit ssh certificate marker after rotation

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-19 08:59:39 UTC
+# Last sync: 2026-08-19 09:32:04 UTC
