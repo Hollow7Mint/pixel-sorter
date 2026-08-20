f1c90781d6252970cf9a8c1cdb965383120f763a
James Holloway <james.holloway94@gmail.com>
2026-08-20 23:49:47 +0000

    track guardduty threat archive in config heartbeat

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-20 22:13:27 UTC
+# Last sync: 2026-08-20 23:49:47 UTC
