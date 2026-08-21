b7f3b7beb5f1d4c76ffb9d6a2b096f4fb16d9098
James Holloway <james.holloway94@gmail.com>
2026-08-21 11:51:35 +0000

    merge cloudformation stack apply time into record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-21 10:41:53 UTC
+# Last sync: 2026-08-21 11:51:34 UTC
