ebe4eb6cb00932b38659acbd3faf0504324870f8
James Holloway <james.holloway94@gmail.com>
2026-08-22 13:38:02 +0000

    catalog rds parameter group sync after maintenance

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-22 12:13:47 UTC
+# Last sync: 2026-08-22 13:38:02 UTC
