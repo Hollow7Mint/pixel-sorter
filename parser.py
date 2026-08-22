1239d54e3f87a0f48fccd27e1503196baa394c4c
James Holloway <james.holloway94@gmail.com>
2026-08-22 15:31:29 +0000

    snapshot bastion session time for security audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-22 14:35:56 UTC
+# Last sync: 2026-08-22 15:31:29 UTC
