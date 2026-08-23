c10f6342b402b99e6bc69c8f43280f119bd00670
James Holloway <james.holloway94@gmail.com>
2026-08-23 05:38:36 +0000

    transfer ssh config time to bastion banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-23 04:49:04 UTC
+# Last sync: 2026-08-23 05:38:36 UTC
