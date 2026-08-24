d4e8483044144d18b36e909a2272cb3f5cd18ef6
James Holloway <james.holloway94@gmail.com>
2026-08-24 22:35:56 +0000

    post postgres replication heartbeat to config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-24 21:27:07 UTC
+# Last sync: 2026-08-24 22:35:56 UTC
