a6a92ec140981fdcb2551c122aaf2a5996bbedb4
James Holloway <james.holloway94@gmail.com>
2026-08-25 06:50:18 +0000

    frame mysql connection limit sync in config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-25 05:41:47 UTC
+# Last sync: 2026-08-25 06:50:18 UTC
