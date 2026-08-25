7208d4f893488031654fd069141b43547aca92a1
James Holloway <james.holloway94@gmail.com>
2026-08-25 03:08:17 +0000

    write cloudtrail log delivery stamp to metadata

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-25 01:59:27 UTC
+# Last sync: 2026-08-25 03:08:17 UTC
