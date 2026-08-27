832ed60453aa8513b74db3a26ae321aed9f70605
James Holloway <james.holloway94@gmail.com>
2026-08-27 15:13:58 +0000

    log shield ddos event time in protection banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-27 14:17:23 UTC
+# Last sync: 2026-08-27 15:13:58 UTC
