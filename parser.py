32c5bae67064b7b158a9d5c8a2c5516649c28c18
James Holloway <james.holloway94@gmail.com>
2026-08-27 11:04:54 +0000

    stage rds connection pool sync for scheduled refresh

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-27 09:47:31 UTC
+# Last sync: 2026-08-27 11:04:54 UTC
