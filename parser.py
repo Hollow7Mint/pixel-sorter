a5d5005b8669bad5073e144be7d75c5dcadb3dea
James Holloway <james.holloway94@gmail.com>
2026-08-27 17:41:41 +0000

    flag cloudwatch log retention sync in metadata

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-27 16:57:48 UTC
+# Last sync: 2026-08-27 17:41:41 UTC
