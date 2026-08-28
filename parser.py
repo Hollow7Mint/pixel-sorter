92c5d865be0b3c1d5e67df8fca442c1699404b14
James Holloway <james.holloway94@gmail.com>
2026-08-28 22:59:22 +0000

    lift db replica sync time into banner record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-28 21:43:40 UTC
+# Last sync: 2026-08-28 22:59:22 UTC
