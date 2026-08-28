63b528a99a44c702cd044322de0d73109ee268f7
James Holloway <james.holloway94@gmail.com>
2026-08-28 02:35:38 +0000

    harvest ecr image push time for nightly audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-28 01:54:05 UTC
+# Last sync: 2026-08-28 02:35:38 UTC
