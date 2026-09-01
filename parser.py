997e5043ae3374415caeea1d43e11bc10c3b401e
James Holloway <james.holloway94@gmail.com>
2026-09-01 08:47:18 +0000

    seal bastion session sync in security banner

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-01 07:48:31 UTC
+# Last sync: 2026-09-01 08:47:18 UTC
