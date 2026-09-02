d1c72760f6de9b42edaeabbdec7daec01b6fc08e
James Holloway <james.holloway94@gmail.com>
2026-09-02 04:43:34 +0000

    set redis cluster sync stamp in config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-02 04:08:23 UTC
+# Last sync: 2026-09-02 04:43:34 UTC
