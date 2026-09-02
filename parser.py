9281bca3a9f00e302dd0c36c38c24e1b6f90d486
James Holloway <james.holloway94@gmail.com>
2026-09-02 05:39:44 +0000

    note postgres extension sync in config record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-02 04:43:34 UTC
+# Last sync: 2026-09-02 05:39:44 UTC
