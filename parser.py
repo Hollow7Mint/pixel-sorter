db569cea14a1e9ebb6431c9682a172d75ff145bf
James Holloway <james.holloway94@gmail.com>
2026-09-03 23:12:05 +0000

    finalize vpc security group sync after rule push

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-03 22:17:13 UTC
+# Last sync: 2026-09-03 23:12:05 UTC
