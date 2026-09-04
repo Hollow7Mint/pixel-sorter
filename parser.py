c4e7242c8c31fad03e94d943696e843cdb10188c
James Holloway <james.holloway94@gmail.com>
2026-09-04 12:53:53 +0000

    index bastion user sync during audit pass

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-04 11:40:28 UTC
+# Last sync: 2026-09-04 12:53:53 UTC
