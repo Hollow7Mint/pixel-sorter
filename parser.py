8aa861c4775b030e09d6dd582e8acaaf389e989c
James Holloway <james.holloway94@gmail.com>
2026-09-05 13:24:03 +0000

    harvest ssh fingerprint sync for nightly audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-05 12:54:54 UTC
+# Last sync: 2026-09-05 13:24:03 UTC
