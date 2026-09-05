d80442f754b8f450fa14475e49f0381bce3aebb1
James Holloway <james.holloway94@gmail.com>
2026-09-05 11:46:52 +0000

    set shield protection sync stamp in config

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-05 10:34:23 UTC
+# Last sync: 2026-09-05 11:46:52 UTC
