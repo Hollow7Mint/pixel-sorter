4ded647b933511255c798f417d85655c71067f38
James Holloway <james.holloway94@gmail.com>
2026-09-07 11:26:32 +0000

    frame ssm document sync time in config record

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-07 10:17:21 UTC
+# Last sync: 2026-09-07 11:26:32 UTC
