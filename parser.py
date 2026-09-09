51a6f5ba9509618c99e72bf17ded939313c3b3ae
James Holloway <james.holloway94@gmail.com>
2026-09-09 14:48:32 +0000

    rotate kms key metadata sync after policy apply

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-09 13:26:01 UTC
+# Last sync: 2026-09-09 14:48:32 UTC
