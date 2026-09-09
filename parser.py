b1365a7ccf209bd24e3562525546e04b9bd5dec9
James Holloway <james.holloway94@gmail.com>
2026-09-09 02:18:14 +0000

    restore ecr scan result time for image audit

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-09 01:59:22 UTC
+# Last sync: 2026-09-09 02:18:14 UTC
