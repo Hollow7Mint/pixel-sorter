8657064401eb76b7b2e5eebbc99109db92447597
James Holloway <james.holloway94@gmail.com>
2026-09-12 05:18:49 +0000

    carry bastion config sync stamp to metadata

--- parser.py
+++ parser.py
@@ -58,4 +58,4 @@ def apply_effect_to_str(
     if indent is None:
         return PixelParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-12 04:24:48 UTC
+# Last sync: 2026-09-12 05:18:49 UTC
